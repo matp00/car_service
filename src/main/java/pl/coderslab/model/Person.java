b32b6663e4787337eac29398a574ab6f65bd7de8
matp00 <sonseeh@gmail.com>
2018-07-03 14:58:26 +0200

    all models

--- src/main/java/pl/coderslab/model/Person.java
+++ src/main/java/pl/coderslab/model/Person.java
@@ -1,4 +1,30 @@
 package pl.coderslab.model;
 
 public class Person {
+    private String name;
+    private String surname;
+
+    public Person(String name, String surname) {
+        setName(name);
+        setSurname(surname);
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
